30804b799a1cac4cce0bed54ba7211f5e84ba783
Rifa Prasetya <rifaekaprasetya13@gmail.com>
2025-11-30 15:26:55 +0700

    up ke 2

--- src/index.js
+++ src/index.js
@@ -10,7 +10,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 app.use(express.json({limit: '2mb'}));
 app.use(express.urlencoded({extended: true}));
 
